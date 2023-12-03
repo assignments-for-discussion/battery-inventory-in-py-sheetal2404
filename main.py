026d79eba172d5d5c508b6ebdb7567c2eaaa16ca
sheetal2404 <112376465+sheetal2404@users.noreply.github.com>
2023-12-03 17:18:50 +0530

    Update main.py
    Implemented the function for counting of battery life

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 
 def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+  # creating dictionary for the classification 
+  classify = {"healthy": 0,
+               "exchange": 0,
+               "failed": 0}
+
+  #claculating the SoH for each 'present capacity'
+  for i in present_capacities:
+    SoH = 100 * i / 120
+    if SoH >= 80 and SoH <=100:
+      classify["healthy"] += 1
+    elif SoH >= 62 and SoH < 80:
+      classify["exchange"] += 1
+    else:
+      classify["failed"] += 1
+
+  return classify
+
 
 
 def test_bucketing_by_health():
